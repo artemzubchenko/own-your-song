6923dd8c689a3e8901e05b09b90936a95da09650
subwayrisotto <109352069+artemzubchenko@users.noreply.github.com>
2025-01-11 00:06:30 +0100

    create an empty project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is an own-your-song page
+      </header>
+    </div>
+  );
+}
+
+export default App;
